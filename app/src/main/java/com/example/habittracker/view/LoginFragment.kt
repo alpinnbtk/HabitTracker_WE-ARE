17237d2ebc32d2d40a34d0e6e0c337bd4922dc5a
alpinnbtk <ambrosiusalvin20@gmail.com>
2026-05-08 21:13:44 +0700

    Delete Default Comment from Android Studio

--- app/src/main/java/com/example/habittracker/view/LoginFragment.kt
+++ app/src/main/java/com/example/habittracker/view/LoginFragment.kt
@@ -11,17 +11,8 @@ import com.example.habittracker.R
 import com.example.habittracker.databinding.FragmentLoginBinding
 import com.example.habittracker.model.UserData
 
-
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-
     private lateinit var binding: FragmentLoginBinding
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
